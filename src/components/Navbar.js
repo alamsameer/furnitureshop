6c81249cd4938f2789bdf4918db7e2dbe7e7b70e
Sameer Alam <alamsameer738@gmail.com>
2022-10-05 00:50:57 +0530

    changed navbar styling 01

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ import AuthState from "./context/AuthState";
 function ShowtoLoginSignup() {
     return (
         <>
-            <Link to="/login" className="hover:text-orange-700   px-3 py-2 md:px-4 md:py-3 md:text-base text-sm font-medium">Login</Link>
-            <Link to="/signup" className="hover:text-orange-700   px-3 py-2 md:px-4 md:py-3 md:text-base text-sm font-medium">signUp</Link>
+            <Link to="/login" className="hover:text-orange-700  hidden md:inline-block   md:px-4 md:py-3 md:text-base text-sm font-medium">Login</Link>
+            <Link to="/signup" className="hover:text-orange-700  hidden md:inline-block  md:px-4 md:py-3 md:text-base text-sm font-medium">signUp</Link>
 
         </>)
 }
@@ -79,18 +79,18 @@ function Nav() {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex gap-16 justify-center place-items-center">
-                            <div className="flex gap-4">
+                        <div className="flex gap-4 md:gap-16 justify-center place-items-center">
+                            <div className="flex gap-2 md:gap-4">
                                 {
                                     user ? <LogedinUser user={user} /> : <ShowtoLoginSignup />
                                 }
-                                <Link to="/cart" className="hover:text-orange-700 md:mt-1 px-3 py-2 text-sm md:text-base md:px-4 md:py-2 font-medium">Basket</Link>
+                                <Link to="/cart" className=" hidden md:inline-block hover:text-orange-700 md:mt-1 px-3 py-2 text-sm md:text-base md:px-4 md:py-2 font-medium">Basket</Link>
                             </div>
                             <div className="-mr-2 flex md:hidden">
                                 <button
                                     onClick={() => setIsOpen(!isOpen)}
                                     type="button"
-                                    className="bg-orange-700 inline-flex items-center justify-center p-2 text-gray-200 hover:text-white hover:bg-orange-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                    className="bg-orange-500 inline-flex items-center justify-center p-2 text-gray-200 hover:text-white hover:bg-orange-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                     aria-controls="mobile-menu"
                                     aria-expanded="false"
                                 >
@@ -148,31 +148,35 @@ function Nav() {
                             <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                                 <Link
                                     to="#"
-                                    className="  hover:bg-orange-700 hover:text-white block px-3 py-2 text-base font-medium"
+                                    className="  hover:bg-orange-400 hover:text-white block px-3 py-2 text-base font-medium"
                                 >
                                     Home
                                 </Link>
 
                                 <Link
                                     to="#"
-                                    className="  hover:bg-orange-700 hover:text-white block px-3 py-2 text-base font-medium"
+                                    className="  hover:bg-orange-400 hover:text-white block px-3 py-2 text-base font-medium"
                                 >
                                     Shop
                                 </Link>
 
                                 <Link
                                     to="#"
-                                    className="  hover:bg-orange-700 hover:text-white block px-3 py-2 text-base font-medium"
+                                    className="  hover:bg-orange-400 hover:text-white block px-3 py-2 text-base font-medium"
                                 >
                                     Contact
                                 </Link>
 
                                 <Link
                                     to="#"
-                                    className="  hover:bg-orange-700 hover:text-white block px-3 py-2 text-base font-medium"
+                                    className="  hover:bg-orange-400 hover:text-white block px-3 py-2 text-base font-medium"
                                 >
                                     Abouts
                                 </Link>
+                                <Link to="/cart" className="  hover:bg-orange-400 hover:text-white block px-3 py-2 text-base font-medium">Basket</Link>
+                                <Link to="/login" className="hover:bg-orange-400 hover:text-white block px-3 py-2 text-base font-medium">Login</Link>
+                                <Link to="/signup" className="hover:bg-orange-400 hover:text-white block px-3 py-2 text-base font-medium">signUp</Link>
+
                             </div>
                         </div>
                     )}
